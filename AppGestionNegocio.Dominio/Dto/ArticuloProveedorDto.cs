fddf74c3a34fc87cdd56fcb252b343da20232e08
Cristian Sanchez <c.sanchezmdp@gmail.com>
2026-06-29 08:26:01 -0300

    Dto necesarios para mapear solo las propiedades que me interesan enviar o recibir del front

---
+++ AppGestionNegocio.Dominio/Dto/ArticuloProveedorDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppGestionNegocio.Dominio
+{
+    public class ArticuloProveedorDto
+    {
+        public int IdArticulo { get; set; }
+        public int IdAlicuotaIva { get; set; }
+        public string Nombre { get; set; }
+        public decimal PrecioUnitario { get; set; }
+        public int Stock { get; set; }
+    }
+}
