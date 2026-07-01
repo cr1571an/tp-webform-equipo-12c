4b6a3316e5f84448eaf906b4424fa94a913f0a6d
Ulises Aguirre <Aguirre.ulises2981@gmail.com>
2026-07-01 12:11:50 -0300

    Vista de listado de ventas con filtros y acciones

--- AppGestionNegocio.Web/Ventas.aspx.cs
+++ AppGestionNegocio.Web/Ventas.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AppGestionNegocio.Dominio;
+using AppGestionNegocio.Negocio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +13,58 @@ namespace AppGestionNegocio.Web
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                CargarGridView();
+            }
+        }
 
+        private void CargarGridView()
+        {
+            VentaNegocio negocio = new VentaNegocio();
+
+            Session["listaVentas"] = negocio.listar();
+
+            dgvVentas.DataSource = Session["listaVentas"];
+            dgvVentas.DataBind();
+        }
+
+        protected void dgvVentas_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            dgvVentas.PageIndex = e.NewPageIndex;
+            dgvVentas.DataSource = Session["listaVentas"];
+            dgvVentas.DataBind();
+        }
+
+        protected void ddlFiltro_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            VentaNegocio negocio = new VentaNegocio();
+            string criterio = ddlFiltro.SelectedValue;
+
+            Session["listaVentas"] = negocio.listarOrdenadoPorFecha(criterio);
+
+            dgvVentas.DataSource = Session["listaVentas"];
+            dgvVentas.DataBind();
+        }
+        protected void btnNuevaVenta_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("VentaFormulario.aspx");
+        }
+
+        protected void dgvVentas_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+        }
+
+        protected void btnVerDetalle_Click(object sender, EventArgs e)
+        {
+        }
+
+        protected void btnEliminar_Click(object sender, EventArgs e)
+        {
+        }
+
+        protected void btnConfirmarEliminar_Click(object sender, EventArgs e)
+        {
         }
     }
 }
--- AppGestionNegocio.Web/Ventas.aspx.designer.cs
+++ AppGestionNegocio.Web/Ventas.aspx.designer.cs
@@ -13,5 +13,50 @@ namespace AppGestionNegocio.Web
 
     public partial class Ventas
     {
+
+        /// <summary>
+        /// Control ddlFiltro.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlFiltro;
+
+        /// <summary>
+        /// Control btnNuevaVenta.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnNuevaVenta;
+
+        /// <summary>
+        /// Control dgvVentas.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView dgvVentas;
+
+        /// <summary>
+        /// Control hfIdVentaEliminar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hfIdVentaEliminar;
+
+        /// <summary>
+        /// Control btnConfirmarEliminar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnConfirmarEliminar;
     }
 }
