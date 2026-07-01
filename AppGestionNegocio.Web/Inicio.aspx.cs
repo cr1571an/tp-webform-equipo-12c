fef6420531a1f5524c41cd129b6146ee98305cc3
Aldana Firpo <firpoaldana@gmail.com>
2026-07-01 07:20:07 -0300

    Aldana/inicio reportes (#19)
    * ABM clientes
    
    * Implementar reportes en inicio
    
    ---------
    
    Co-authored-by: Aldana Firpo <firpoaldana@gmail.comgit>

--- AppGestionNegocio.Web/Inicio.aspx.cs
+++ AppGestionNegocio.Web/Inicio.aspx.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.Data;
+using System.Globalization;
+using AppGestionNegocio.Negocio;
 
 namespace AppGestionNegocio.Web
 {
@@ -11,7 +9,71 @@ namespace AppGestionNegocio.Web
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                cargarReportes();
+                cargarActividadReciente();
+            }
+        }
 
+        private void cargarReportes()
+        {
+            try
+            {
+                InicioNegocio negocio = new InicioNegocio();
+
+                CultureInfo culturaArgentina = new CultureInfo("es-AR");
+
+                decimal totalVentasDia = negocio.obtenerTotalVentasDia();
+                int cantidadVentasDia = negocio.obtenerCantidadVentasDia();
+
+                decimal totalComprasDia = negocio.obtenerTotalComprasDia();
+                int cantidadComprasDia = negocio.obtenerCantidadComprasDia();
+
+                int cantidadArticulos = negocio.obtenerCantidadArticulosActivos();
+                int cantidadClientes = negocio.obtenerCantidadClientesActivos();
+
+                lblTotalVentasDia.Text = totalVentasDia.ToString("C", culturaArgentina);
+                lblCantidadVentasDia.Text = cantidadVentasDia == 1 ? "1 venta" : cantidadVentasDia + " ventas";
+
+                lblTotalComprasDia.Text = totalComprasDia.ToString("C", culturaArgentina);
+                lblCantidadComprasDia.Text = cantidadComprasDia == 1 ? "1 compra" : cantidadComprasDia + " compras";
+
+                lblCantidadArticulos.Text = cantidadArticulos.ToString();
+                lblCantidadClientes.Text = cantidadClientes.ToString();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void cargarActividadReciente()
+        {
+            try
+            {
+                InicioNegocio negocio = new InicioNegocio();
+
+                DataTable tablaActividad = negocio.listarActividadReciente();
+
+                if (tablaActividad.Rows.Count > 0)
+                {
+                    rptActividadReciente.DataSource = tablaActividad;
+                    rptActividadReciente.DataBind();
+
+                    pnlActividadVacia.Visible = false;
+                    pnlActividadLista.Visible = true;
+                }
+                else
+                {
+                    pnlActividadVacia.Visible = true;
+                    pnlActividadLista.Visible = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
